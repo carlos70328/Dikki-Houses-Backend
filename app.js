655202b8068d7a15047936890bde6b9ff0954cd9
Kevin Agudelo <kevinagugall@gmail.com>
2020-03-27 21:18:52 -0500

    add cors

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const createError = require('http-errors');
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-global.containerDependency = require('./containerDependency/Container');
-
 const indexRouter = require('./routes/index');
 const housesRouter = require('./routes/houses');
-
 const app = express();
+
+global.containerDependency = require('./containerDependency/Container');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 containerDependency.get('mongooseDriver').connect();
 
+// ---------------- Routes ----------------
 app.use('/', indexRouter);
 app.use('/houses', housesRouter);
 
