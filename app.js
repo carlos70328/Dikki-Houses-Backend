950ec4b6e72f5a6144d82aca63912377f56897ab
Kevin Agudelo <kevinagugall@gmail.com>
2020-06-06 11:09:20 -0500

    implementing deno

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
-global.containerDependency = require('./containerDependency/Container');
-global.Helpers = require('./helpers/Helpers');
+import { serve } from "https://deno.land/std@0.55.0/http/server.ts";
+const s = serve({ port: 8000 });
 
-const express = require('express');
-const cors = require('cors');
+console.log("http://localhost:8000/");
 
-const createError = require('http-errors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./src/routes/index');
-const housesRouter = require('./src/routes/houses');
-const app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
-containerDependency.get('mongooseDriver').connect();
-
-// ---------------- Routes ----------------
-app.use('/', indexRouter);
-app.use('/houses', housesRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
-
-module.exports = app;
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
