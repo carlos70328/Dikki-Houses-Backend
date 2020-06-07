a2578552d4b0660796bd6cce9e13d5d7fed27607
Kevin Agudelo <kevinagugall@gmail.com>
2020-06-07 15:18:27 -0500

    delete me for serv er

--- deleteMe.js
+++ deleteMe.js
@@ -1,15 +1,46 @@
-var cloudinary = require('cloudinary').v2
 
-cloudinary.uploader.upload("./public/test-pattern.jpg", 
-  { 
-    "cloud_name": "dth8mghha",
-    "api_key": "126434579775861", 
-    "api_secret": "EFEGmL3Vwn7hZ0-ARNCIeJUzJTw",
-    "public_id": "Houses/",
-    "overwrite": true
-  },
-  (error, result) => {
-    console.log("KERROR", error)
-    console.log("KRESULT", result) 
-  }
-);
+require('dotenv').config()
+const express = require('express')
+const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
+
+const app = express()
+
+cloudinary.config({ 
+  cloud_name: "dth8mghha", 
+  api_key: "126434579775861", 
+  api_secret: "EFEGmL3Vwn7hZ0-ARNCIeJUzJTw"
+})
+  
+app.use(formData.parse())
+
+app.post('/image-upload', (req, res) => {
+  console.log("ENTRARON");
+
+  const values = Object.values(req.files)
+  console.log(values);
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+})
+
+app.listen(process.env.PORT || 5000, () => console.log('👍'))
+
+
+// var cloudinary = require('cloudinary').v2
+
+// cloudinary.uploader.upload("./public/test-pattern.jpg", 
+//   { 
+//     "cloud_name": "dth8mghha",
+//     "api_key": "126434579775861", 
+//     "api_secret": "EFEGmL3Vwn7hZ0-ARNCIeJUzJTw",
+//     "public_id": "Houses/",
+//     "overwrite": true
+//   },
+//   (error, result) => {
+//     console.log("KERROR", error)
+//     console.log("KRESULT", result) 
+//   }
+// );
