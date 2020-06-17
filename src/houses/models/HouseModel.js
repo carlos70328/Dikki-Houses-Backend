b0606072f1e619da6ff642654a7007856b004f96
Kevin Agudelo <kevinagugall@gmail.com>
2020-06-16 22:11:14 -0500

    Trying to add jest

--- src/houses/models/HouseModel.js
+++ src/houses/models/HouseModel.js
@@ -3,7 +3,7 @@ const ModelInterface = require('../interfaces/models/ModelInterface');
 class HouseModel extends ModelInterface {
 
     /**
-     * 
+     * Define house model
      * @param {DatabaseInterface} driver : driver to make actions in database
      * @param {string} modelName : name to representate the object
      * @param {HouseSchema} houseSchema : valid schema for create model
