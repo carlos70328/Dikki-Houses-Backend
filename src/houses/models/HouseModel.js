b8defe14a7d2a8808184aef3c182fa937bdf6f13
Carlos Florez <carlosflo70328@gmail.com>
2022-04-08 10:37:40 -0500

    add Params

--- src/houses/models/HouseModel.js
+++ src/houses/models/HouseModel.js
@@ -1,69 +1,75 @@
-const ModelInterface = require('../interfaces/models/ModelInterface');
+const ModelInterface = require("../interfaces/models/ModelInterface");
 
 class HouseModel extends ModelInterface {
+   /**
+    * Define house model
+    * @param {DatabaseInterface} driver : driver to make actions in database
+    * @param {string} modelName : name to representate the object
+    * @param {HouseSchema} houseSchema : valid schema for create model
+    */
+   constructor(driver, modelName, schema) {
+      super(driver, modelName, schema);
+      this.createModel();
+   }
 
-    /**
-     * Define house model
-     * @param {DatabaseInterface} driver : driver to make actions in database
-     * @param {string} modelName : name to representate the object
-     * @param {HouseSchema} houseSchema : valid schema for create model
-     */
-    constructor(driver, modelName, schema){
-        super(driver, modelName, schema);
-        this.createModel();
-    }
+   /**
+    * Save info in database
+    * @param {Object} info : If it has geoposition  convert to db format
+    */
+   saveInfo(info) {
+      if (info.geoPosition) {
+         info.geoPosition = this.driver.convertPositon(info.geoPosition);
+      }
+      return super.saveInfo(info);
+   }
 
-    /**
-     * Save info in database
-     * @param {Object} info : If it has geoposition  convert to db format
-     */
-    saveInfo(info){
-        if(info.geoPosition){
-            info.geoPosition = this.driver.convertPositon(info.geoPosition);
-        }
-        return super.saveInfo(info);
-    }
+   editInfo(filter, update) {
+      if (update.geoPosition) {
+         update.geoPosition = this.driver.convertPositon(update.geoPosition);
+      }
 
-    editInfo(filter, update){
-        if(update.geoPosition){
-            update.geoPosition = this.driver.convertPositon(update.geoPosition);
-        }
+      return super.editInfo(filter, update);
+   }
 
-        return super.editInfo(filter, update);
-    }
-
-    /**
-     * Search houses from X,Y point in a Radius
-     * @param {Array<number>} coordinates: First position [0] is X or Latitude; Second position [1] is Y or Longitude
-     * @param {number} maxDistance: Radios in Meters for search
-     * @param {Object} filter: aditional filter for search by location
-     * @param {string} selectValues: select values
-     */
-    findByLocation(coordinates, maxDistance, filter = {}, selectValues = '') {
-        const searchByLocation =  {
-            geoPosition: {
-                $near: {
-                    $maxDistance: maxDistance,
-                    $geometry: {
-                        type: constants.houseConst.geoLocationPoint,
-                        coordinates: [coordinates.x, coordinates.y]
-                    }
-                }
+   /**
+    * Search houses from X,Y point in a Radius
+    * @param {Array<number>} coordinates: First position [0] is X or Latitude; Second position [1] is Y or Longitude
+    * @param {number} maxDistance: Radios in Meters for search
+    * @param {Object} filter: aditional filter for search by location
+    * @param {string} selectValues: select values
+    */
+   findByLocation(coordinates, maxDistance, filter = {}, selectValues = "") {
+      const searchByLocation = {
+         geoPosition: {
+            $near: {
+               $maxDistance: maxDistance,
+               $geometry: {
+                  type: constants.houseConst.geoLocationPoint,
+                  coordinates: [coordinates.lat, coordinates.lng],
+               },
             },
-            ...filter
-        };
+         },
+         ...filter,
+      };
 
-        return new Promise((resolve, reject) => {
-            this.driver.findByFilter(this._model, searchByLocation, selectValues, (err, info) => {
-                if(err) {
-                    reject({ status: constants.httpConst.CLIENT_ERROR, error: err });
-                }
-                else {
-                    resolve({ status: constants.httpConst.OK, info: info });
-                }
-            });
-        });
-    }
+      return new Promise((resolve, reject) => {
+         this.driver.findByFilter(
+            this._model,
+            searchByLocation,
+            selectValues,
+            (err, info) => {
+               if (err) {
+                  reject({
+                     status: constants.httpConst.CLIENT_ERROR,
+                     error: err,
+                  });
+               } else {
+                  resolve({ status: constants.httpConst.OK, info: info });
+               }
+            }
+         );
+      });
+   }
 }
 
 module.exports = HouseModel;
--- src/houses/routes/houses.js
+++ src/houses/routes/houses.js
@@ -9,111 +9,114 @@ const imageManager = containerDependency.get("imageService");
 const paramBuilder = containerDependency.get("houseParamBuilder");
 
 router.get("/", ({ query }, res, next) => {
-  const findParams = paramBuilder.setParams(query);
-  const paginationParams = paramBuilder.setPagination(query);
+   const findParams = paramBuilder.setParams(query);
+   const paginationParams = paramBuilder.setPagination(query);
 
-  houseModel
-    .find(findParams, paginationParams, HousesResponse.showAllHouses)
-    .then(({ info, status }) => {
-      res.status(status).json(info);
-    })
-    .catch(({ error, status }) => {
-      res.status(status).json(error);
-    });
+   houseModel
+      .find(findParams, paginationParams, HousesResponse.showAllHouses)
+      .then(({ info, status }) => {
+         res.status(status).json(info);
+      })
+      .catch(({ error, status }) => {
+         res.status(status).json(error);
+      });
 });
 
 router.get("/search", ({ query }, res, next) => {
-  const findParams = paramBuilder.setParams(query);
-  const paginationParams = paramBuilder.setPagination(query);
+   const findParams = paramBuilder.setParams(query);
+   const paginationParams = paramBuilder.setPagination(query);
 
-  console.log(findParams);
-  console.log(paginationParams);
+   console.log(findParams);
+   console.log(paginationParams);
 
-  houseModel
-    .find(findParams, paginationParams, HousesResponse.showAllHouses)
-    .then(({ info, status }) => {
-      res.status(status).json(info);
-    })
-    .catch(({ error, status }) => {
-      res.status(status).json(error);
-    });
+   houseModel
+      .find(findParams, paginationParams, HousesResponse.showAllHouses)
+      .then(({ info, status }) => {
+         res.status(status).json(info);
+      })
+      .catch(({ error, status }) => {
+         res.status(status).json(error);
+      });
 });
 
 router.get("/geolocation", (req, res, next) => {
-  const coordinates = { x: req.query.x, y: req.query.y };
-  const maxDistance = req.query.maxDistance;
-  const filter = req.query.filter;
+   const coordinates = { lat: req.query.lng, lat: req.query.lng };
+   const maxDistance = req.query.maxDistance;
+   const filter = req.query.filter;
 
-  houseModel
-    .findByLocation(
-      coordinates,
-      maxDistance,
-      filter,
-      HousesResponse.showAllHouses
-    )
-    .then(({ info, status }) => {
-      res.status(status).json(info);
-    })
-    .catch(({ error, status }) => {
-      res.status(status).json(error);
-    });
+   houseModel
+      .findByLocation(
+         coordinates,
+         maxDistance,
+         filter,
+         HousesResponse.showAllHouses
+      )
+      .then(({ info, status }) => {
+         res.status(status).json(info);
+      })
+      .catch(({ error, status }) => {
+         res.status(status).json(error);
+      });
 });
 
 router.get("/show/:id", (req, res, next) => {
-  const id = req.params.id; //sh3yfelxzkyvuh9cb3q8 for test
+   const id = req.params.id; //sh3yfelxzkyvuh9cb3q8 for test
 
-  houseModel
-    .findById(id, HousesResponse.showSpecificHouse)
-    .then(({ info, status }) => {
-      res.status(status).json(info);
-    })
-    .catch(({ error, status }) => {
-      res.status(status).json(error);
-    });
+   houseModel
+      .findById(id, HousesResponse.showSpecificHouse)
+      .then(({ info, status }) => {
+         res.status(status).json(info);
+      })
+      .catch(({ error, status }) => {
+         res.status(status).json(error);
+      });
 });
 
 //No olvidar añadir el token
 router.post("/add_houses", (req, res, next) => {
-  const dataToSave = req.body;
-  dataToSave.public_id = uid(20);
+   const dataToSave = req.body;
+   dataToSave.public_id = uid(20);
 
-  try {
-    houseModel
-      .saveInfo(dataToSave)
-      .then(({ info, status }) => {
-        res.status(status).json({ public_id: info.public_id });
-      })
-      .catch(({ error, status }) => {
-        res.status(status).json(error);
-      });
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
+   try {
+      houseModel
+         .saveInfo(dataToSave)
+         .then(({ info, status }) => {
+            res.status(status).json({ public_id: info.public_id });
+         })
+         .catch(({ error, status }) => {
+            res.status(status).json(error);
+         });
+   } catch (error) {
+      res.status(400).json(error.message);
+   }
 });
 
 router.post("/add_house_images", async (req, res, next) => {
-  const imagesToSave = Object.values(req.files);
-  const userId = req.body.userId;
-  const houseId = req.body.houseId;
-  const folder = `users/${userId}/houses/${houseId}/`;
+   const imagesToSave = Object.values(req.files);
+   const userId = req.body.userId;
+   const houseId = req.body.houseId;
+   const folder = `users/${userId}/houses/${houseId}/`;
 
-  const images = await imageManager.uploadImages(imagesToSave, folder);
-  const transformImage = await houseModel.editInfo({ public_id: houseId }, { resources: { photos: images.info }});
-  res.status(200).json(transformImage);
+   const images = await imageManager.uploadImages(imagesToSave, folder);
+   const transformImage = await houseModel.editInfo(
+      { public_id: houseId },
+      { resources: { photos: images.info } }
+   );
+   res.status(200).json(transformImage);
 });
 
 router.post("/edit_house", (req, res, next) => {
-  const filter = req.body.filter;
-  const dataUpdate = req.body.update;
+   const filter = req.body.filter;
+   const dataUpdate = req.body.update;
 
-  houseModel
-    .editInfo(filter, dataUpdate)
-    .then(({ info, status }) => {
-      res.status(status).json(info);
-    })
-    .catch(({ error, status }) => {
-      res.status(status).json(error);
-    });
+   houseModel
+      .editInfo(filter, dataUpdate)
+      .then(({ info, status }) => {
+         res.status(status).json(info);
+      })
+      .catch(({ error, status }) => {
+         res.status(status).json(error);
+      });
 });
 
 // router.post('/add_houses', authorization, (req, res, next) => {
@@ -123,7 +126,7 @@ router.post("/edit_house", (req, res, next) => {
 // });
 
 router.get("/verifyToken", authorization, (req, res, next) => {
-  res.send("Token is ok");
+   res.send("Token is ok");
 });
 
 module.exports = router;
